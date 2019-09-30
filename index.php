220da56515ac8971523aa91cfde5abe1310cf95f
KatFuss <55210338+KatFuss@users.noreply.github.com>
2019-09-30 04:41:18 -0400

    php includes/menu highlighter

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+		<title>Assignment 3: Information Architecture</title>
+		<link rel="stylesheet" href="css/styles.css">
+	</head>
+
+	<body>
+
+		<div id="left"></div>
+		<div id="right"></div>
+		<div id="top"></div>
+		<div id="bottom"></div>
+
+		<header class="center">
+			<a href="index.php">
+				<h1>Team Quito</h1>
+				<img src="images/s_quito.jpg" alt="Quito, Ecuador">
+			</a>
+		</header>
+
+		<?php include "inc/index-nav.inc"; ?>
+
+		<main>
+			<p>Hello World! We are Team Quito. Jennifer Ngene is our Informational Architect, Clara Martinez Rubio is our Visual Designer, and Kat Fuss is our Technical Coder.</p>
+
+			<p>With this website, we hope to give the viewers an in-depth look at some of the students that go to the University of Rochester. Rochester prides itself for its inclusion of undergraduates and graduates of different cultural backgrounds. You'll get to learn more about their journeys to the university and their humble beginnings from where they grew up as well as their future plans. Click on their links to learn more!</p>
+		</main>
+
+<?php include "inc/footer.inc"; ?>
